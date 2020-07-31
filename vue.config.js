dd6a5d394311fd2ba4a414b2c6c9548e212da9c5
Drew Harper <druharper@gmail.com>
2020-07-31 11:27:13 -0500

    cors exact

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   devServer: {
-    proxy: {
-      '/api': {
-        target: 'https://10.0.0.13:8081',
-        secure: false,
-        changeOrigin: true
-      }
-    }
+    port: 8081
   }
 }
